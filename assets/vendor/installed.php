272eea5afabae15fc69f2246c58fdaaa0bedd85c
Kevin <44907984+kevinwekulo@users.noreply.github.com>
2025-05-07 14:38:33 +0200

    material

---
+++ assets/vendor/installed.php
@@ -0,0 +1,22 @@
+<?php return array (
+  'materialize-css' => 
+  array (
+    'version' => '1.0.0',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+  'materialize-css/dist/css/materialize.min.css' => 
+  array (
+    'version' => '1.0.0',
+    'dependencies' => 
+    array (
+    ),
+    'extraFiles' => 
+    array (
+    ),
+  ),
+);
